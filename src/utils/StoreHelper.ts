c570e31aed226e98df77bfe934bb5866189cc65a
lichao <184078718@qq.com>
2022-11-07 10:47:13 +0800

    aaa

--- src/utils/StoreHelper.ts
+++ src/utils/StoreHelper.ts
@@ -1,5 +1,5 @@
 // 设置自定义iframe大屏是否显示
-export const bigScreenVisibleSet = (bigScreen, Routers) => {
+export const bigScreenVisibleSet = (bigScreen:any, Routers:any) => {
   if (bigScreen.length > 0) {
     for (const item of Routers) {
       if (item.name === 'sysExtendPage') {
