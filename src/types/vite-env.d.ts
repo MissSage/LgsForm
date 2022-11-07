6ca828346bcbd7a8f73f735aa52739030b6e6be3
lichao <184078718@qq.com>
2022-11-07 17:30:53 +0800

    aaa

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
-
+import FormIns from '../packages/components/Form.vue'
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+declare module '@missssage/lgsform'{
+  export const Form: typeof FormIns
 }
