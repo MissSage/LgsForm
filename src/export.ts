eaf49efbe8cc794acd866789bbc058f122964d01
lichao <184078718@qq.com>
2022-11-07 15:28:17 +0800

    fix: export

--- src/export.ts
+++ src/export.ts
@@ -1,15 +1,13 @@
 import { App } from 'vue'
-import Button from './packages/components/Button.vue'
-import AttrTable from './packages/components/AttrTable.vue'
-import Form from './packages/components/Form.vue'
+import Form from './packages/es/Form'
 
-const components = [Button, AttrTable, Form]
+const components = [Form]
 
 const install = (app: App): void => {
   components.map(component => app.component(component.name, component))
 }
 export {
-  Button,AttrTable,Form
+  Form
 }
 export default {
   install
--- src/packages/es/Form.ts
+++ src/packages/es/Form.ts
@@ -1,7 +1,8 @@
 import {App} from 'vue'
 import Form from '../components/Form.vue'
-
+type SFCWithInstall<T> = T & { install?:(app: App)=> void };
 Form.install = (app: App):void=> {
   app.component(Form.name, Form)
 }
-export default Form
+const FormInstall: SFCWithInstall<typeof Form> = Form
+export default FormInstall
