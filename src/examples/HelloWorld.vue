f1b54556cba3e0ab12ddfa8f895b60a93ee133c9
lichao <184078718@qq.com>
2022-11-08 23:52:08 +0800

    组件存在问题，待修复

--- src/examples/HelloWorld.vue
+++ src/examples/HelloWorld.vue
@@ -1,20 +1,25 @@
 <template>
   <Form ref="refForm" :config="FormConfig" />
+  <FormTable :config="TableConfig"></FormTable>
 </template>
 <script lang="ts" setup>
-import { reactive, ref } from 'vue';
+import { reactive, ref } from "vue";
 // import Form from '../packages/es/Form'
-import {Form } from '../../dist/lgsform.js'
-import { IFormConfig } from '../../global';
-const a = ref<InstanceType<typeof Form>>()
+import FormTable from "../packages/components/FormTable.vue";
+import Form from "../packages/components/Form.vue";
+import { IFormConfig, ITable } from "../../global";
 const FormConfig = reactive<IFormConfig>({
   group: [
     {
-      fields: [
-        {type: 'input',field: 'aaa',label: 'bbb',}
-      ]
-    }
-  ]
-})
-
+      fields: [{ type: "input", field: "aaa", label: "bbb" }],
+    },
+  ],
+});
+const TableConfig = reactive<ITable>({
+  dataList: [{ index: "111" }],
+  pagination: {
+    hide: true,
+  },
+  columns: [{ label: "序号", prop: "index" }],
+});
 </script>
--- src/packages/components/FormItem.vue
+++ src/packages/components/FormItem.vue
@@ -1,44 +1,32 @@
 <template>
-  <label
-    v-if="config.type === 'text'"
-    class="readonly-input"
-  >
+  <label v-if="config.type === 'text'" class="readonly-input">
     {{
-      (config.formatter ? config.formatter(modelValue, row) : modelValue) || '-'
+      (config.formatter ? config.formatter(modelValue, row) : modelValue) || "-"
     }}
-    {{ config.unit ? config.unit : '' }}
+    {{ config.unit ? config.unit : "" }}
   </label>
   <Input
     v-else-if="
       config.type === 'input' ||
-        config.type === 'password' ||
-        config.type === 'textarea'
+      config.type === 'password' ||
+      config.type === 'textarea'
     "
     v-model="state.value"
     :config="config"
     :size="computedSize"
-    @blur="e => $emit('blur', state.value, e)"
+    @blur="(e) => $emit('blur', state.value, e)"
     @change="handleChange"
-    @focus="e => $emit('focus', state.value, e)"
+    @focus="(e) => $emit('focus', state.value, e)"
     @clear="() => $emit('clear', state.value)"
   >
-    <template
-      v-if="config.suffix"
-      #suffix
-    >
+    <template v-if="config.suffix" #suffix>
       <span v-if="config.unit">{{ config.unit }}</span>
       <span v-else>{{ config.suffix }}</span>
     </template>
-    <template
-      v-if="config.prefix"
-      #prefix
-    >
+    <template v-if="config.prefix" #prefix>
       <span>{{ config.prefix }}</span>
     </template>
-    <template
-      v-if="config.prepend"
-      #prepend
-    >
+    <template v-if="config.prepend" #prepend>
       <span v-if="typeof config.prepend === 'string'">{{
         config.prepend
       }}</span>
@@ -51,7 +39,7 @@
               ? config.prepend.style(state.prependVal, row, config)
               : config.prepend?.style
           "
-          @change="val => onChange(config.prepend, val, config)"
+          @change="(val) => onChange(config.prepend, val, config)"
         >
           <el-option
             v-for="(obj, i) in config.prepend?.options"
@@ -92,7 +80,7 @@
               ? config.append.style(state.appendVal, row, config)
               : config.append?.style
           "
-          @change="val => onChange(config.append, val, config)"
+          @change="(val) => onChange(config.append, val, config)"
         >
           <el-option
             v-for="(obj, i) in config.append?.options"
@@ -119,23 +107,14 @@
     onkeyup="value=value.replace(/[^\d.]/g, '').replace('.', '$#$').replace(/\./g, '').replace('$#$', '.')"
     @change="handleChange"
   >
-    <template
-      v-if="config.suffix || config.unit"
-      #suffix
-    >
+    <template v-if="config.suffix || config.unit" #suffix>
       <span v-if="config.unit">{{ config.unit }}</span>
       <span v-else>{{ config.suffix }}</span>
     </template>
-    <template
-      v-if="config.prefix"
-      #prefix
-    >
+    <template v-if="config.prefix" #prefix>
       <span>{{ config.prefix }}</span>
     </template>
-    <template
-      v-if="config.prepend"
-      #prepend
-    >
+    <template v-if="config.prepend" #prepend>
       <span v-if="typeof config.prepend === 'string'">{{
         config.prepend
       }}</span>
@@ -148,7 +127,7 @@
               ? config.prepend.style(state.prependVal, row, config)
               : config.prepend?.style
           "
-          @change="val => onChange(config.prepend, val, config)"
+          @change="(val) => onChange(config.prepend, val, config)"
         >
           <el-option
             v-for="(obj, i) in config.prepend?.options"
@@ -159,10 +138,7 @@
         </el-select>
       </template>
     </template>
-    <template
-      v-if="config.append"
-      #append
-    >
+    <template v-if="config.append" #append>
       <span v-if="typeof config.append === 'string'">{{ config.append }}</span>
       <template v-else>
         <el-select
@@ -173,7 +149,7 @@
               ? config.append.style(state.appendVal, row, config)
               : config.append?.style
           "
-          @change="val => onChange(config.append, val, config)"
+          @change="(val) => onChange(config.append, val, config)"
         >
           <el-option
             v-for="(obj, i) in config.append?.options"
@@ -208,7 +184,7 @@
     v-model="state.value"
     :size="computedSize"
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :class="{ 'no-border': config.noBorder }"
     filterable
@@ -237,8 +213,8 @@
         config.disableOption
           ? config.disableOption(obj, state.value, row)
           : typeof obj.disabled === 'function'
-            ? obj.disabled(state.value, row, obj)
-            : obj.disabled
+          ? obj.disabled(state.value, row, obj)
+          : obj.disabled
       "
     />
     <!-- </template> -->
@@ -247,7 +223,7 @@
     v-else-if="config.type === 'cascader'"
     v-model="state.value"
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :clearable="config.clearable !== false"
     :size="computedSize"
@@ -260,7 +236,7 @@
     :options="(config.options as any)"
     :props="
       config.props || {
-        checkStrictly: true
+        checkStrictly: true,
       }
     "
     @change="handleChange"
@@ -269,7 +245,7 @@
     v-else-if="config.type === 'select-tree'"
     v-model="state.value"
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :size="computedSize"
     :data="config.options"
@@ -289,7 +265,7 @@
         children: 'children',
         disabled: 'disabled',
         isLeaf: 'isLeaf',
-        class: 'class'
+        class: 'class',
       }
     "
     :disabled="
@@ -329,7 +305,7 @@
     :size="computedSize"
     :style="{
       ...(config.style || {}),
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :disabled="
       typeof config.readonly === 'function'
@@ -366,21 +342,16 @@
               ? config.prefixStyle(kv, state.value, row, config)
               : config.prefixStyle
           "
-        >{{
-          typeof config.prefix === 'function'
-            ? config.prefix(kv, state.value, row, config)
-            : config.prefix
-        }}</span>
+          >{{
+            typeof config.prefix === "function"
+              ? config.prefix(kv, state.value, row, config)
+              : config.prefix
+          }}</span
+        >
         <span>
           <el-icon v-if="kv.icon || kv.svgIcon">
-            <i
-              v-if="kv.icon"
-              :class="kv.icon"
-            ></i>
-            <component
-              :is="kv.svgIcon"
-              v-else
-            ></component>
+            <i v-if="kv.icon" :class="kv.icon"></i>
+            <component :is="kv.svgIcon" v-else></component>
           </el-icon>
           {{ kv.label }}
         </span>
@@ -392,11 +363,12 @@
               ? config.suffixStyle(kv, state.value, row, config)
               : config.suffixStyle
           "
-        >{{
-          typeof config.suffix === 'function'
-            ? config.suffix(kv, state.value, row, config)
-            : config.suffix
-        }}</span>
+          >{{
+            typeof config.suffix === "function"
+              ? config.suffix(kv, state.value, row, config)
+              : config.suffix
+          }}</span
+        >
       </el-radio-button>
     </template>
   </el-radio-group>
@@ -431,12 +403,12 @@
         : config.readonly
     "
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :size="computedSize"
     :type="config.type"
     :placeholder="config.placeholder || '请选择'"
-    :disabled-date="val => getDateOptions(val)"
+    :disabled-date="(val) => getDateOptions(val)"
     :format="config.textFormat || 'YYYY-MM'"
     :value-format="config.format || 'YYYY-MM'"
     @change="handleChange"
@@ -451,12 +423,12 @@
         : config.readonly
     "
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :size="computedSize"
     :type="config.type"
     :placeholder="config.placeholder || '请选择'"
-    :disabled-date="val => getDateOptions(val)"
+    :disabled-date="(val) => getDateOptions(val)"
     :format="config.textFormat || 'YYYY'"
     :value-format="config.format || 'YYYY'"
     @change="handleChange"
@@ -471,35 +443,35 @@
         : config.readonly
     "
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :size="computedSize"
     :type="config.type"
     :placeholder="config.placeholder || '请选择'"
-    :disabled-date="val => getDateOptions(val)"
+    :disabled-date="(val) => getDateOptions(val)"
     :format="
       config.textFormat ||
-        (config.type === 'date' ? 'YYYY-MM-DD' : 'YYYY-MM-DD HH:mm:ss')
+      (config.type === 'date' ? 'YYYY-MM-DD' : 'YYYY-MM-DD HH:mm:ss')
     "
     :value-format="
       config.format
         ? config.format
         : config.type === 'date'
-          ? 'YYYY-MM-DD'
-          : 'YYYY-MM-DD HH:mm:ss'
+        ? 'YYYY-MM-DD'
+        : 'YYYY-MM-DD HH:mm:ss'
     "
     @change="handleChange"
   />
   <el-date-picker
     v-else-if="
       config.type === 'daterange' ||
-        config.type === 'datetimerange' ||
-        config.type === 'monthrange'
+      config.type === 'datetimerange' ||
+      config.type === 'monthrange'
     "
     v-model="state.value"
     style="padding: 0 10px"
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     :size="computedSize"
     :clearable="config.clearable !== false"
@@ -513,13 +485,13 @@
         : config.readonly
     "
     :placeholder="config.placeholder || '请选择'"
-    :disabled-date="val => getDateOptions(val)"
+    :disabled-date="(val) => getDateOptions(val)"
     :value-format="
       config.format
         ? config.format
         : config.type === 'daterange'
-          ? 'YYYY-MM-DD'
-          : 'YYYY-MM-DD HH:mm:ss'
+        ? 'YYYY-MM-DD'
+        : 'YYYY-MM-DD HH:mm:ss'
     "
     :format="config.textFormat"
     @change="handleChange"
@@ -536,7 +508,7 @@
       :size="computedSize"
       :clearable="config.clearable !== false"
       :placeholder="config.placeholder || '请选择'"
-      :disabled-date="val => pickerStartAuditYear(val)"
+      :disabled-date="(val) => pickerStartAuditYear(val)"
       value-format="YYYY"
       style="width: 100px"
     >
@@ -549,7 +521,7 @@
       :size="computedSize"
       :clearable="config.clearable !== false"
       :placeholder="config.placeholder || '请选择'"
-      :disabled-date="val => pickerEndAuditYear(val)"
+      :disabled-date="(val) => pickerEndAuditYear(val)"
       value-format="YYYY"
       style="width: 100px"
     >
@@ -561,7 +533,7 @@
     v-model="state.value"
     placeholder="请选择时间"
     :style="{
-      width: config.width || '100%'
+      width: config.width || '100%',
     }"
     range-separator="至"
     start-placeholder="开始时间"
@@ -592,7 +564,8 @@
         ? config.style(state.value, row, config)
         : config.style
     "
-  >{{ config?.text }}</span>
+    >{{ config?.text }}</span
+  >
   <AvatarUploader
     v-else-if="config.type === 'avatar'"
     v-model="state.value"
@@ -616,32 +589,17 @@
         : config.style
     "
   >
-    <template
-      v-for="(item, i) in config.btns"
-      :key="i"
-    >
+    <template v-for="(item, i) in config.btns" :key="i">
       <component
         :is="item.component"
         v-if="item.component"
         :config="item"
         :row="row"
       ></component>
-      <el-tooltip
-        v-else-if="item.title"
-        :content="item.title"
-      >
-        <Button
-          :config="item"
-          :row="row"
-          :size="computedSize"
-        ></Button>
+      <el-tooltip v-else-if="item.title" :content="item.title">
+        <Button :config="item" :row="row" :size="computedSize"></Button>
       </el-tooltip>
-      <Button
-        v-else
-        :config="item"
-        :row="row"
-        :size="computedSize"
-      ></Button>
+      <Button v-else :config="item" :row="row" :size="computedSize"></Button>
     </template>
   </div>
   <Tabs
@@ -691,10 +649,7 @@
     :config="config"
     @change="handleChange"
   />
-  <div
-    v-else-if="config.type === 'range'"
-    style="width: 100%"
-  >
+  <div v-else-if="config.type === 'range'" style="width: 100%">
     <RangeSelecter
       v-model="state.value"
       :config="config"
@@ -722,7 +677,7 @@
   >
     {{
       (config.formatter && config.formatter(state.value, row, config)) ||
-        state.value
+      state.value
     }}
   </Tag>
   <div
@@ -745,7 +700,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive, watch } from 'vue'
+import { computed, reactive, watch } from "vue";
 import {
   ElInput,
   ElSelect,
@@ -762,67 +717,68 @@ import {
   ElCheckbox,
   ElDatePicker,
   ElTimePicker,
-  ElTooltip
-} from 'element-plus'
-import TagGroup from './TagGroup.vue'
-import AvatarUploader from './AvatarUploader.vue'
-import { deresolveValue, resolveValue } from '../isValid'
-import { onChange, nodeClick } from '@/utils/FormHelper'
-import Button from './Button.vue'
-import RangeSelecter from './RangeSelecter.vue'
-import ColorPicker from './ColorPicker.vue'
-import FormWangeditor from './FormWangeditor.vue'
-import List from './List.vue'
-import TiniImageUploader from './TiniImageUploader.vue'
-import FormTree from './FormTree.vue'
-import Tabs from './Tabs.vue'
-import { IFormDate, IFormDateRange, IFormItem, ISize } from '../../../global'
-import Input from './Input.vue'
+  ElTooltip,
+} from "element-plus";
+import TagGroup from "./TagGroup.vue";
+import AvatarUploader from "./AvatarUploader.vue";
+import { deresolveValue, resolveValue } from "../isValid";
+import { onChange, nodeClick } from "@/utils/FormHelper";
+import Button from "./Button.vue";
+import RangeSelecter from "./RangeSelecter.vue";
+import ColorPicker from "./ColorPicker.vue";
+import FormWangeditor from "./FormWangeditor.vue";
+import List from "./List.vue";
+import TiniImageUploader from "./TiniImageUploader.vue";
+import FormTree from "./FormTree.vue";
+import Tabs from "./Tabs.vue";
+import Tag from "./Tag.vue";
+import { IFormDate, IFormDateRange, IFormItem, ISize } from "../../../global";
+import Input from "./Input.vue";
 // import IconSelector from './IconSelector.vue'
 const props = defineProps<{
-  modelValue?: string | number | Array<any> | Record<string, any> | boolean
-  row?: Record<string, any>
-  size?: ISize
-  config: IFormItem
-  width?: string
-}>()
-const emit = defineEmits(['update:model-value', 'change'])
+  modelValue?: string | number | Array<any> | Record<string, any> | boolean;
+  row?: Record<string, any>;
+  size?: ISize;
+  config: IFormItem;
+  width?: string;
+}>();
+const emit = defineEmits(["update:model-value", "change"]);
 const computedSize = computed(() => {
-  return props.config.size || props.size || 'default'
-})
+  return props.config.size || props.size || "default";
+});
 const state = reactive<{
-  value: any
-  prependVal?: any
-  appendVal?: any
-  imgActionUrl: string
-  fileActionUrl: string
+  value: any;
+  prependVal?: any;
+  appendVal?: any;
+  imgActionUrl: string;
+  fileActionUrl: string;
 }>({
-  imgActionUrl: '',
-  fileActionUrl: '',
+  imgActionUrl: "",
+  fileActionUrl: "",
   value: resolveValue(props.config, props.modelValue),
   prependVal: props.config.prependDefault,
-  appendVal: props.config.appendDefault
-})
+  appendVal: props.config.appendDefault,
+});
 
 const handleChange = (val: any) => {
-  emit('update:model-value', deresolveValue(props.config, val))
-  emit('change', val, props.row, props.config)
+  emit("update:model-value", deresolveValue(props.config, val));
+  emit("change", val, props.row, props.config);
   // props.config.onChange && props.config.onChange(val, props.row, props.config, state.options)
-}
+};
 const setoptionval = computed(() => {
-  if (props.config.type === 'select') {
-    if (!props.row || !props.config.setOptionBy) return ''
-    return props.row[props.config.setOptionBy]
+  if (props.config.type === "select") {
+    if (!props.row || !props.config.setOptionBy) return "";
+    return props.row[props.config.setOptionBy];
   }
-  return ''
-})
+  return "";
+});
 watch(
   () => setoptionval.value,
   async () => {
-    if (props.config.type === 'select' && props.config.setOptionBy) {
+    if (props.config.type === "select" && props.config.setOptionBy) {
       // state.value = undefined
       if (props.config.setOptionMethod) {
-        props.config.setOptionMethod(props.config, props.row)
+        props.config.setOptionMethod(props.config, props.row);
         // state.options = res
       } else {
         // state.options = []
@@ -830,64 +786,64 @@ watch(
     }
   },
   { immediate: true }
-)
+);
 const getDateOptions = (date: any) => {
-  const item = props.config as IFormDate | IFormDateRange
+  const item = props.config as IFormDate | IFormDateRange;
   // 2021.07.17设置时间可选范围
   if ((!item.min && !item.max) || !date) {
-    return false
+    return false;
   }
-  if (item.min && item.min.indexOf(' ') === -1) {
+  if (item.min && item.min.indexOf(" ") === -1) {
     // 不设置时分秒，后面会自动加上 08:00
-    item.min += ' 00:00:000'
+    item.min += " 00:00:000";
   }
-  return compareDate(date, item.min) || !compareDate(date, item.max)
-}
-const filterNodeMethod = (value, data) => data.label?.includes(value)
+  return compareDate(date, item.min) || !compareDate(date, item.max);
+};
+const filterNodeMethod = (value, data) => data.label?.includes(value);
 const compareDate = (date1: any, date2: any) => {
   if (!date2) {
-    return true
+    return true;
   }
   return (
-    date1.valueOf()
-    < (typeof date2 === 'number' ? date2 : new Date(date2).valueOf())
-  )
-}
+    date1.valueOf() <
+    (typeof date2 === "number" ? date2 : new Date(date2).valueOf())
+  );
+};
 watch(
   () => state.value,
   (val: string) => {
-    emit('update:model-value', val)
+    emit("update:model-value", val);
     // emit('change', val, props.row, props.config,state.options)
     // props.config.onChange && props.config.onChange(val, props.row, props.config,state.options)
   },
   {
-    deep: true
+    deep: true,
   }
-)
+);
 watch(
   () => props.modelValue,
-  val => {
-    const newVal = resolveValue(props.config, val)
-    state.value = newVal
+  (val) => {
+    const newVal = resolveValue(props.config, val);
+    state.value = newVal;
     // if (val !== newVal) {
     // }
   },
   {
-    immediate: true
+    immediate: true,
   }
-)
+);
 
 // 年份区间相关
 
-const pickerStartAuditYear = value => {
-  console.log(value)
+const pickerStartAuditYear = (value) => {
+  console.log(value);
 
   // state.value[1] = null  开启开始年份清空结束年份
-}
+};
 
-const pickerEndAuditYear = value => {
-  return value.getFullYear() < parseInt(state.value[0])
-}
+const pickerEndAuditYear = (value) => {
+  return value.getFullYear() < parseInt(state.value[0]);
+};
 </script>
 
 <style lang="scss" scoped>
