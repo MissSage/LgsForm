34387b1e482d89d5ebe43fb540a1c178a4910d18
lichao <184078718@qq.com>
2023-01-09 14:56:02 +0800

    增加游戏目录

--- docs/.vitepress/navBar/index.ts
+++ docs/.vitepress/navBar/index.ts
@@ -36,6 +36,10 @@ const navs: DefaultTheme.NavItem[] = [
           {
             text: "六角形",
             link: "https://hextris.io/"
+          },
+          {
+            text: '贪吃蛇（地址栏版',
+            link: 'https://demian.ferrei.ro/snake'
           }
         ]
       }
@@ -57,12 +61,37 @@ const navs: DefaultTheme.NavItem[] = [
         text: "工具箱",
         items: [
           {
+            text: "mysql",
+            link: "/utils/mysql"
+          }
+        ]
+      },
+      {
+        text: "奇怪的工具箱",
+        items: [
+          {
             text: "表情包生成器",
             link: "https://meme.itswincer.com/dagong/"
           },
           {
-            text: "mysql",
-            link: "/utils/mysql"
+            text: "毒鸡汤",
+            link: "http://www.nows.fun/"
+          },
+          {
+            text: "狗屁不通文章生成器",
+            link: "https://suulnnka.github.io/BullshitGenerator/index.html"
+          },
+          {
+            text: "亲戚关系计算器",
+            link: "https://passer-by.com/relationship/"
+          },
+          {
+            text: '程序员的女装空间',
+            link: 'https://drsrel.github.io/photos'
+          },
+          {
+            text: '英语肌肉记忆锻炼',
+            link: 'https://kaiyiwing.gitee.io/qwerty-learner/'
           }
         ]
       }
